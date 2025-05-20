0452b5d77fc8a02fd4f1a627ddcc6f50a4876328
zhangli <2475934003@qq.com>
2025-05-20 20:53:16 +0800

    feat:c-build InitPlugin初始化webpack配置(部分)

--- c-build/plugins/init-plugin.js
+++ c-build/plugins/init-plugin.js
@@ -1,5 +1,6 @@
 const path = require('node:path');
 const logger = require('../lib/utils/logger');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = function (ctx, params) {
     logger.info({
         prefix: 'InitPlugin',
@@ -21,7 +22,7 @@ module.exports = function (ctx, params) {
     config.mode(buildMode);
     logger.info({
         prefix: 'InitPlugin',
-        message: `set webpack building mode: ${buildMode}`,
+        message: `set webpack building mode completed: ${buildMode}`,
     })
 
     // 设置entry
@@ -31,7 +32,7 @@ module.exports = function (ctx, params) {
         .end();
     logger.info({
         prefix: 'InitPlugin',
-        message: `set webpack entry point, index: './src/index.js'`,
+        message: `set webpack entry point completed, index: './src/index.js'`,
     })
 
     // 设置output
@@ -42,10 +43,71 @@ module.exports = function (ctx, params) {
         .end();
     logger.info({
         prefix: 'InitPlugin',
-        message: `set webpack output, dist: '[name]_[hash].js'`,
+        message: `set webpack output completed, dist: '[name]_[hash].js'`,
     })
 
-    // 设置module loader配置
+    // 设置module css 相关 loader配置
+    config
+        .module
+        .rule('css')
+        .test(/\.css$/)
+        .exclude
+        .add(/node_modules/)
+        .end()
+        .use('mini-css')
+        .loader(MiniCssExtractPlugin.loader)
+        .end()
+        .use('css-loader')
+        .loader('css-loader')
+        .end();
+    logger.info({
+        prefix: 'InitPlugin',
+        message: `set webpack module css related loader completed: MiniCssExtractPlugin.loader + 'css-loader'`,
+    })
+
+    // 设置 module 图片相关 loader配置
+    config
+       .module
+       .rule('images')
+       .test(/\.(png|svg|jpe?g|gif|webp)(\?.*)?$/)
+        .type('asset')
+        .parser({
+            dataUrlCondition: {
+                maxSize: 3 * 1024 // 10kb
+            }
+        })
+        .generator({
+            filename: 'images/[name].[hash].[ext]',
+        })
+        .end();
+    logger.info({
+        prefix: 'InitPlugin',
+        message: `set webpack module asset related loader completed`,
+    })
+
+    // 设置 module ejs相关 loader配置
+    config
+        .module
+        .rule('ejs')
+        .test(/\.ejs$/)
+        .use('ejs-loader')
+        .options({
+            esModule: false
+        })
+        .end();
+    logger.info({
+        prefix: 'InitPlugin',
+        message: `set webpack module ejs related loader completed: ejs-loader`,
+    })
+
+
+
+
+
+    logger.verbose({
+        prefix: 'InitPlugin webpackConfig.module.rules',
+        message: config.toConfig().module.rules,
+    })
 
     logger.verbose({
         prefix: 'InitPlugin finished webpack config',
